1fccd23a88a30148a5e59494e078fb36e269990d
dmarck82 <douglas.marcondes@gmail.com>
2022-04-16 18:10:58 -0300

    Adicionado mais um tratamento de exceções

--- src/main/java/br/edu/utfpr/tdsapi/tdsapi/exceptionhandler/TdsExceptionHandler.java
+++ src/main/java/br/edu/utfpr/tdsapi/tdsapi/exceptionhandler/TdsExceptionHandler.java
@@ -1,11 +1,13 @@
 package br.edu.utfpr.tdsapi.tdsapi.exceptionhandler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -59,6 +62,16 @@ public class TdsExceptionHandler extends ResponseEntityExceptionHandler{
 
         List<Erro> erros = criarListadeErros(ex.getBindingResult());
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler({EmptyResultDataAccessException.class})
+    protected ResponseEntity<Object> handleEmptyResultDataAcessException(EmptyResultDataAccessException ex, WebRequest request){
+
+        String menssagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+        String menssagemDesenvolvedor = ex.toString();
+        
+        List<Erro> erros = Arrays.asList(new Erro(menssagemUsuario, menssagemDesenvolvedor));
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     private List<Erro> criarListadeErros(BindingResult bindingResult){
